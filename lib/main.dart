35739598ff260c42b99ba33d951ad9d1c8c4cb8d
guptaad <guptaad@google.com>
2018-07-17 20:10:43 +0100

    Adding source.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_shopping_cart/shopping_cart_app.dart';
 
 void main() {
+  // originally https://github.com/pszklarska/FlutterShoppingCart.git
   // this app follows from the guide:
   // https://hackernoon.com/flutter-redux-how-to-make-shopping-list-app-1cd315e79b65#f4b5
 
